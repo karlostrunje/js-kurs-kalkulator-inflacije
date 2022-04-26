a326b803305da4ec9f721fe0e2e82f8c34452de5
Karlo Strunje <karlostrunje96@gmail.com>
2022-04-26 09:38:28 +0200

    Added script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function inflationCalculator() {
+    let inflationRate = document.querySelector("#inflationRate");
+    let money = document.querySelector("#money");
+
+    inflationRate = parseFloat(inflationRate.value);
+    money = parseFloat(money.value);
+
+   let years = document.querySelector("#years").value;
+   years = parseFloat(years);
+
+   // Formula za izračunavanje inflacije
+   let worth = money + (money * (inflationRate / 100));
+
+   for (let i = 1; i < years; i++){
+       worth += worth * (inflationRate/100);
+   }
+
+   worth = worth.toFixed(2);
+
+   let newElement = document.createElement("div");
+   newElement.className = "new-value";
+   newElement.innerText = `Današnjih ${money}€ vrijedi isto kao ${worth} za ${years} godina `
+   document.querySelector(".container").appendChild(newElement);
+
+}
